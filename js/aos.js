fe6a68466f3c7235cc434e190565f1254bd7096f
Mohamed Ahmed <125946649+mohamed5580@users.noreply.github.com>
2025-11-28 15:30:07 +0200

    fix

--- js/aos.js
+++ js/aos.js
@@ -1,1 +1,14 @@
 AOS.init();
+document.addEventListener("DOMContentLoaded", function () {
+  const items = document.querySelectorAll(".Resume_page .item");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("shown");
+      }
+    });
+  }, { threshold: 0.3 }); // 30% visible
+
+  items.forEach(item => observer.observe(item));
+});
---
+++ js/main.js
@@ -0,0 +1,14 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const btn = document.querySelector(".main_btn");
+//   const menu = document.querySelector(".show-L");
+
+//   btn.addEventListener("click", () => {
+//     if (menu.style.opacity === "1") {
+//       menu.style.opacity = "0";
+//       menu.style.pointerEvents = "none";
+//     } else {
+//       menu.style.opacity = "1";
+//       menu.style.pointerEvents = "auto";
+//     }
+//   });
+// });
