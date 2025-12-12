24775b1adfdd77a43a6984147403e62d5be58010
Mohamed Ahmed <125946649+mohamed5580@users.noreply.github.com>
2025-12-12 20:24:05 +0200

    add popup

--- js/main.js
+++ js/main.js
@@ -37,3 +37,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
     window.addEventListener("scroll", highlightNav);
 });
+
+function openPopup(popup) {
+    /* Open popup and make accessible screen readers */
+    $(popup).show().attr("aria-hidden", "false");
+    /* Focus on element to guide screen readers to popup */
+    $("#closePopup").focus();
+}
+
+function closePopup(popup) {
+    /* Close popup and hide from screen readers */
+    $(popup).hide().attr("aria-hidden", "true");
+    /* Focus screen readers back to button */
+    $("#openMyPopup").focus();
+}
+
+
+
+$(".modal-wide").on("show.bs.modal", function () {
+    var height = $(window).height() - 200;
+    $(this).find(".modal-body").css("max-height", height);
+});
