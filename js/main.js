e74f43287b4f41ca488127bfa763e2cb9fc8d2d5
Mohamed Ahmed <125946649+mohamed5580@users.noreply.github.com>
2026-05-18 23:08:02 +0300

    New update

--- js/main.js
+++ js/main.js
@@ -16,24 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const sections = document.querySelectorAll(".name_pages");
     const navLinks = document.querySelectorAll(".nav-link");
 
-    function highlightNav() {
-        let scrollY = window.pageYOffset;
 
-        sections.forEach(current => {
-            const sectionHeight = current.offsetHeight;
-            const sectionTop = current.offsetTop - 150;
-            const sectionId = current.getAttribute("id");
-
-            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-                navLinks.forEach(link => {
-                    link.classList.remove("active");
-                    if (link.getAttribute("href").includes(sectionId)) {
-                        link.classList.add("active");
-                    }
-                });
-            }
-        });
-    }
 
     window.addEventListener("scroll", highlightNav);
 });
